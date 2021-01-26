251ee785d77ed36a1f6af28423e56ae2b0a0d77e
surendranSura <surendranvankat@gmail.com>
2021-01-27 00:47:42 +0530

    new staff module person details design going partially i Modified Header-action.component.css,.html,personal-details.component.html,ts,custom-theme.scss

--- SMS/SMS/ClientApp/src/app/master/components/staff/new-staff/personal-details/personal-details.component.ts
+++ SMS/SMS/ClientApp/src/app/master/components/staff/new-staff/personal-details/personal-details.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Blood {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-personal-details',
   templateUrl: './personal-details.component.html',
   styleUrls: ['./personal-details.component.css']
 })
+
 export class PersonalDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    
+  }
+
+  bloods: Blood[] = [
+    {value: 'AP-0', viewValue: 'A+'},
+    {value: 'BP-1', viewValue: 'B+'},
+    {value: 'ABP-2', viewValue: 'AB+'},
+    {value: 'OP-3', viewValue: 'OP+'}
+  ];  
 
   ngOnInit(): void {
+    
   }
 
 }
