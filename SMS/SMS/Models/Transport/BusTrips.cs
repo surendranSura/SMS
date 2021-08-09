6cf765a456144890eea08cc4aa5ea3e6afd8b991
vikisrepo <60546814+vikisRepo@users.noreply.github.com>
2021-08-10 01:48:33 +0530

    Transport Models add

---
+++ SMS/SMS/Models/Transport/BusTrips.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMS.Models.Transport
+{
+    public class BusTrips
+    {
+        public int BusTripsid { get; set; }
+
+        public int TripNumber { get; set; }
+
+        public string TripAreas { get; set; }
+
+        public TimeSpan TripTimingFrom { get; set; }
+
+        public TimeSpan TripTimingTo { get; set; }
+
+        [ForeignKey("BusNumber")]
+        public int BusNmber { get; set; }
+
+        [ForeignKey("DriverNumber")]
+        public int DriverNumber { get; set; }
+
+        [ForeignKey("DriverName")]
+        public string DriverName { get; set; }
+
+        [ForeignKey("Class")]
+        public string StudentClass { get; set; }
+
+        [ForeignKey("Section")]
+        public string StudentSection { get; set; }
+
+        public string StudentList { get; set; }
+
+
+    }
+}
---
+++ SMS/SMS/Models/Transport/BusesAndDrivers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMS.Models.Transport
+{
+    public class BusesAndDrivers
+    {
+        public int BusesAndDriversid { get; set; }
+
+        public string BusType { get; set; }
+
+        public int SeatCount { get; set; }
+
+        public int BusNumber { get; set; }
+
+        public int InsurancePolicyNum { get; set; }
+
+        public DateTime InsuranceEndDate { get; set; }
+
+        public string NotificationSpan { get; set; }
+
+        public string BusStatus { get; set; }
+
+        public TimeSpan ArrivalTime { get; set; }
+
+        public string DriverName { get; set; }
+
+        public int DriverNumber { get; set; }
+
+        public int DriverAadhar { get; set; }
+
+        public byte BusLocation { get; set; }
+    }
+}
