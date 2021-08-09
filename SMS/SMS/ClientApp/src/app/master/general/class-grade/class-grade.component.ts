250fa405adffee19b99dd9bc7e3993a01e189b00
vikisrepo <60546814+vikisRepo@users.noreply.github.com>
2021-08-10 00:09:21 +0530

    Class API Changes

--- SMS/SMS/ClientApp/src/app/master/general/class-grade/class-grade.component.ts
+++ SMS/SMS/ClientApp/src/app/master/general/class-grade/class-grade.component.ts
@@ -29,6 +29,7 @@ export class ClassGradeComponent implements OnInit {
   constructor(private classGradeRestApiService :ClassGradeRestApiService) { }
 
   ngOnInit(): void {
+    debugger
     this.getData();
   }
 
