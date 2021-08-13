2ee43d78347485ba0dab913c2e94f306f34907f6
surendranSura <surendranvankat@gmail.com>
2021-08-14 02:49:07 +0530

    added class time table view and edit options

--- SMS/SMS/ClientApp/src/app/master/academic/class-timetable/class-timetable.module.ts
+++ SMS/SMS/ClientApp/src/app/master/academic/class-timetable/class-timetable.module.ts
@@ -4,13 +4,20 @@ import { CommonModule } from '@angular/common';
 import { ClassTimetableRoutingModule } from './class-timetable-routing.module';
 import { ClassTimetableComponent } from './class-timetable.component';
 import { TimeTableEditorComponent } from './time-table-editor/time-table-editor.component';
+import { TimeTableViewerComponent } from './time-table-viewer/time-table-viewer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { TimeTableService } from './services/time-table.service';
 
 
 @NgModule({
-  declarations: [ClassTimetableComponent, TimeTableEditorComponent],
+  declarations: [ClassTimetableComponent, TimeTableEditorComponent, TimeTableViewerComponent],
   imports: [
     CommonModule,
-    ClassTimetableRoutingModule
-  ]
+    ClassTimetableRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
+  ],
+  providers:[TimeTableService]
 })
 export class ClassTimetableModule { }
--- SMS/SMS/ClientApp/src/app/master/academic/class-timetable/time-table-editor/time-table-editor.component.ts
+++ SMS/SMS/ClientApp/src/app/master/academic/class-timetable/time-table-editor/time-table-editor.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SmsConstant } from 'src/app/shared/constant-values';
+import { TimeTableService } from '../services/time-table.service';
 
 @Component({
   selector: 'app-time-table-editor',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimeTableEditorComponent implements OnInit {
 
-  constructor() { }
+  subjects = SmsConstant.Subjectsdropdown;
+  staff = SmsConstant.staffName;
+
+  slotForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private ttObj: TimeTableService,
+    public dialogRef: MatDialogRef<TimeTableEditorComponent>) { }
 
   ngOnInit(): void {
+  
+  
+    this.slotForm = this.fb.group({
+      subject: ['', [Validators.required]],
+      staff: ['', [Validators.required]],
+      startTime: ['', [Validators.required]],
+      endTime: ['', [Validators.required]]
+    });
+
+    if(this.data){
+      console.log(this.data)
+    
+      this.slotForm.patchValue(this.data);
+    }
+
   }
+
+  close(val?: any) {
+    if (!val) {
+      this.dialogRef.close();
+    }
+    else {
+
+    }
+  }
+
 
 }
